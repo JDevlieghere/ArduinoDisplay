1edbbf2f1a35872534cd4fa59a89f5f54457da92
Jonas Devlieghere <jonas@devlieghere.com>
2016-11-20 21:45:51 +0100

    Update SerialDisplay.ino

--- Arduino/SerialDisplay/SerialDisplay.ino
+++ Arduino/SerialDisplay/SerialDisplay.ino
@@ -1,59 +1,60 @@
-#include <Wire.h>  // Comes with Arduino IDE
+#include <LiquidCrystal_I2C.h> // From https://bitbucket.org/fmalpartida/new-liquidcrystal
+#include <Wire.h>              // Comes with Arduino IDE
 #include <string.h>
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 #define LCD_ROWS 20
 #define LCD_COLS 4
 
-void setup(){
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+void print();
+void blink();
+void disable();
+void enable();
+
+void setup() {
   Serial.begin(9600);
-  lcd.begin(LCD_ROWS,LCD_COLS);
-  lcd.backlight();
-  lcd.setCursor(7,1);
+  lcd.begin(LCD_ROWS, LCD_COLS);
+  lcd.setCursor(7, 1);
   lcd.print("Ready!");
 }
 
-void loop(){
-  if(Serial.available()){
+void loop() {
+  if (Serial.available()) {
     String received = Serial.readStringUntil('\n');
-    if(received == "_DISABLE"){
+    if (received == "_DISABLE") {
       disable();
-    }else if(received == "_ENABLE"){
+    } else if (received == "_ENABLE") {
       enable();
-    }
-    }else{
+    } else {
       print(received);
     }
   }
 }
 
-void print(String s){
+void print(const String &s) {
   lcd.clear();
   int currentLine = 0;
   int increment = LCD_ROWS;
-  for (int start = 0; start < s.length() ; start += increment){
-    if(currentLine < 4){
-      while(s.substring(start).startsWith(" ")){
-        start++;
-      }
-      String line;
-      if(start + LCD_ROWS > s.length()){
-        line = s.substring(start);
-      }else{
-        line = s.substring(start, start + LCD_ROWS);
-      }
-      lcd.setCursor(0,currentLine++);
-      lcd.print(line);
+  for (int start = 0; start < s.length() && currentLine < 4;
+       start += increment) {
+    while (s.substring(start).startsWith(" ")) {
+      start++;
     }
+    String line;
+    if (start + LCD_ROWS > s.length()) {
+      line = s.substring(start);
+    } else {
+      line = s.substring(start, start + LCD_ROWS);
+    }
+    lcd.setCursor(0, currentLine++);
+    lcd.print(line);
   }
   delay(100);
 }
 
-void blink(int n){
-  for(int i = 0; i<n; i++)
-  {
+void blink(int n) {
+  for (int i = 0; i < n; i++) {
     lcd.backlight();
     delay(250);
     lcd.noBacklight();
@@ -61,12 +62,12 @@ void blink(int n){
   }
 }
 
-void disable(){
+void disable() {
   lcd.noDisplay();
   lcd.noBacklight();
 }
 
-void enable(){
+void enable() {
   lcd.display();
   lcd.backlight();
 }
